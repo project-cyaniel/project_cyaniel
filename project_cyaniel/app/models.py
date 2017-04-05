289a4fdc8be63aa9752c6692f4104f275f2124d4
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-04-05 15:51:14 -0700

    continue work on models

--- project_cyaniel/app/models.py
+++ project_cyaniel/app/models.py
@@ -17,9 +17,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), index=True, unique=True)
     phone = db.Column(db.String(20))
-    username = db.Column(db.String(60), index=True, unique=True)
-    first_name = db.Column(db.String(60), index=True)
-    last_name = db.Column(db.String(60), index=True)
+    user_name = db.Column(db.String(200))
     birth_month = db.Column(db.String(20), index=True)
     birth_day = db.Column(db.Integer, index=True)
     birth_year = db.Column(db.Integer, index=True)
@@ -29,8 +27,8 @@ class User(UserMixin, db.Model):
     emergency_contact_name = db.Column(db.String(60))
     emergency_contact_number = db.Column(db.String(20))
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    player_character = db.relationship('PlayerCharacter', backref='user')
+    user_role_id = db.Column(db.Integer, db.ForeignKey('user_roles.id'))
+    user_character = db.relationship('UserCharacter', backref='user')
     is_admin = db.Column(db.Boolean, default=False)
 
     @property
@@ -55,6 +53,83 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User: {}>'.format(self.username)
+
+
+class UserRole(UserMixin, db.Model):
+    """
+    Create a Player Character table - all characters assigned to user
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'user_roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    user_name = db.Column(db.String(200))
+    role = db.Column(db.String(200), index=True)
+    user = db.Relationship('User', backref='user_role')
+
+
+class UserCharacter(UserMixin, db.Model):
+    """
+    Create a User Character table - all characters assigned to user
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'user_characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(60), index=True)
+    user_name = db.Column(db.String(60), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    character = db.Column('Character', backref='user_character')
+
+
+class Character(UserMixin, db.Model):
+    """
+    Create a Player Character table - all characters assigned to user
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(60), index=True)
+    player_name = db.Column(db.String(60), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+class PlayerCharacter(UserMixin, db.Model):
+    """
+    Create a Player Character table - all characters assigned to user
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'player_character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(60), index=True)
+    player_name = db.Column(db.String(60), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+class PlayerCharacter(UserMixin, db.Model):
+    """
+    Create a Player Character table - all characters assigned to user
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'player_character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(60), index=True)
+    player_name = db.Column(db.String(60), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 class PlayerCharacter(UserMixin, db.Model):
