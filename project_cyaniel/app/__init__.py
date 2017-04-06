da3b58bd76c8afc1a67924e3e20b6fd61ead5d61
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-04-06 09:59:07 -0700

    Added and registered blueprints

--- project_cyaniel/app/__init__.py
+++ project_cyaniel/app/__init__.py
@@ -30,4 +30,16 @@ def create_app(config_name):
 
     from app import models
 
+    # Register all blueprints here
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+
     return app
