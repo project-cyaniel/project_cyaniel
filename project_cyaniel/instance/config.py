cd657a87e2bf4509d563338b5d12caf91780afa5
Spencer McGhin <spencermcghin@users.noreply.github.com>
2017-04-06 12:40:06 -0700

    config file removed. keep locally only.

--- project_cyaniel/instance/config.py
+++
@@ -1,3 +0,0 @@
-
-SECRET_KEY = '\\xcc\\xde\\x8dB}\\xeaw=\\xb6\\x1a>Y\\x91\\xe91\\xf0ZY\\xf9\\xcf\\x8d\\xb2\\xc1\\xf0'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://larpworks_admin:larpworks@larpworks.cy4xjwjlyq1s.us-west-2.rds.amazonaws.com/larpworks_db'
