f1723b5ea73403eea3ff1075afb0b5f6f4410626
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-04-06 12:28:11 -0700

    added registration form and index logo

--- project_cyaniel/app/__init__.py
+++ project_cyaniel/app/__init__.py
@@ -1,16 +1,14 @@
-# Third-party imports
+# third-party imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-# Local imports
+# local imports
 from config import app_config
 
-# db variable initialization
 db = SQLAlchemy()
-
-# login variable initialization
 login_manager = LoginManager()
 
 
@@ -18,9 +16,9 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
-    # Create Log-in manager
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
@@ -30,8 +28,7 @@ def create_app(config_name):
 
     from app import models
 
-    # Register all blueprints here
-
+    # Register blueprints here
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
@@ -40,6 +37,5 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-
 
     return app
